78c035ee690e67c9afd22d342dd702f592b8d470
taichi <taichishiozawa@gmail.com>
2024-06-03 21:03:48 +0900

    add: メール認証を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root "home#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
