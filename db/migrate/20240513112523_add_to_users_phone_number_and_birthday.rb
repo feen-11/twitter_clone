d023363368b68f61d29446fef883ce9758768f24
taichi <taichishiozawa@gmail.com>
2024-06-09 15:18:39 +0900

    update: userのデフォルト値修正

--- db/migrate/20240513112523_add_to_users_phone_number_and_birthday.rb
+++ db/migrate/20240513112523_add_to_users_phone_number_and_birthday.rb
@@ -3,8 +3,8 @@
 class AddToUsersPhoneNumberAndBirthday < ActiveRecord::Migration[7.0]
   def change
     change_table :users, bulk: true do |t|
-      t.string :phone_number, null: false, default: '未設定'
-      t.date :birthday, null: false, default: '1900-01-01'
+      t.string :phone_number, null: false
+      t.date :birthday, null: false
     end
   end
 end
