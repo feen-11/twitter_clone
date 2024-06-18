5721811f5ff7c0399b0d2833296bf76cceaddf48
taichi <taichishiozawa@gmail.com>
2024-06-18 20:50:45 +0900

    add: userのseedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user01 = User.new(
+  name: 'ユーザー1',
+  email: 'user01@email.com',
+  phone_number: '01234567890',
+  birthday: 19981114,
+  password: 'user01',
+  confirmed_at: Time.zone.now
+)
+user01.avatar.attach(io: File.open(Rails.root.join('app/assets/images/seed/user/avatar_default.png')),
+                       filename: 'avatar_default.png')
+user01.save!
+
+user02 = User.new(
+  name: 'ユーザー2',
+  email: 'user02@email.com',
+  phone_number: '12345678901',
+  birthday: 19981115,
+  password: 'user02',
+  confirmed_at: Time.zone.now
+)
+user02.avatar.attach(io: File.open(Rails.root.join('app/assets/images/seed/user/avatar_default.png')),
+                       filename: 'avatar_default.png')
+user02.save!
