b298fd0f8d94446155495ac7ff1f9a4788c382a6
taichi <taichishiozawa@gmail.com>
2024-07-01 22:30:22 +0900

    add: commentモデル関連のアソシエーションを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :user
+  has_many_attached :images
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,6 @@ class Post < ApplicationRecord
   has_many :liking_users, through: :likes, source: :user
   has_many :reposts, dependent: :destroy
   has_many :reposting_users, through: :reposts, source: :user
+  has_many :comments, dependent: :destroy
+  has_many :commenting_users, through: :comments, source: :user
 end
