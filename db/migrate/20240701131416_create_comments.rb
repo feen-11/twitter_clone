3cf49394da131519ebb348943ea91ebeb0c82134
taichi <taichishiozawa@gmail.com>
2024-07-01 22:22:36 +0900

    add: commentテーブル追加

---
+++ db/migrate/20240701131416_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.references :user, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: { to_table: :posts }
+      t.text :content, null: false, default: ''
+
+      t.timestamps
+    end
+
+    add_index :comments, %i[user_id post_id]
+  end
+end
