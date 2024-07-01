6359ceade4ca677ec6157afbb33cc87c6638bc1e
taichi <taichishiozawa@gmail.com>
2024-07-01 11:20:06 +0900

    add: user#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users, only: [:show]
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     omniauth_callbacks: 'users/omniauth_callbacks'
