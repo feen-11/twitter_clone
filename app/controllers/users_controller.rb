94cbc3959a0efb674e1b25beb8dc3db9af08d2a8
taichi <taichishiozawa@gmail.com>
2024-07-01 21:30:16 +0900

    add: いいね欄追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = Post.where(user_id: params[:id]).order(created_at: :desc).includes(user: { avatar_attachment: :blob },
                                                         images_attachments: :blob).page(params[:page]).per(10)
+    @liked_posts = @user.liked_posts.page(params[:page]).per(10)
   end
 
 end
