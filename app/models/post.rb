9b9a8944284b44dbbba8c219588f47afa5114a84
taichi <taichishiozawa@gmail.com>
2024-07-01 21:26:44 +0900

    add: postモデルとuserモデルにアソシエーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
   has_many :likes, dependent: :destroy
+  has_many :liking_users, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :follower_users, through: :followings, source: :follower
   has_many :following_users, through: :followers, source: :following
   has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
   has_one_attached :avatar
   has_one_attached :header
   devise :database_authenticatable, :registerable,
