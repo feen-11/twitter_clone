e99a754535ae9535c7854d0259baa601de5a51c2
taichi <taichishiozawa@gmail.com>
2024-07-04 13:07:46 +0900

    loginしていない時のリダイレクトを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :login_check
   def show
     @user = User.find(params[:id])
     @posts = Post.where(user_id: params[:id]).order(created_at: :desc).includes(user: { avatar_attachment: :blob }, images_attachments: :blob).page(params[:page]).per(10)
