3d57d4eb2e0ef178e99e3f2820b6c32f83691c87
taichi <taichishiozawa@gmail.com>
2024-07-04 13:03:30 +0900

    add: usersコントローラにincludesを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(user_id: params[:id]).order(created_at: :desc).includes(user: { avatar_attachment: :blob },
-                                                                                images_attachments: :blob).page(params[:page]).per(10)
-    @liking_posts = @user.liking_posts.page(params[:page]).per(10)
-    @reposting_posts = @user.reposting_posts.page(params[:page]).per(10)
-    @replied_target_posts = @user.replied_to_posts.page(params[:page]).per(10)
+    @posts = Post.where(user_id: params[:id]).order(created_at: :desc).includes(user: { avatar_attachment: :blob }, images_attachments: :blob).page(params[:page]).per(10)
+    @liking_posts = @user.liking_posts.order(created_at: :desc).includes(user: { avatar_attachment: :blob }, images_attachments: :blob).page(params[:page]).per(10)
+    @reposting_posts = @user.reposting_posts.order(created_at: :desc).includes(user: { avatar_attachment: :blob }, images_attachments: :blob).page(params[:page]).per(10)
+    @replied_target_posts = @user.replied_to_posts.order(created_at: :desc).includes(user: { avatar_attachment: :blob }, images_attachments: :blob).page(params[:page]).per(10)
   end
 end
