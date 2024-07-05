c13eb13d302a78b164db22e488675c61c654c0ae
taichi <taichishiozawa@gmail.com>
2024-07-05 15:18:50 +0900

    fix: リンクを修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,6 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
     # before_action :configure_sign_up_params, only: [:create]
-    # before_action :configure_account_update_params, only: [:update]
 
     # GET /resource/sign_up
 
@@ -18,9 +17,9 @@ module Users
     # end
 
     # PUT /resource
-    def update
-      super
-    end
+    # def update
+    #   super
+    # end
 
     # DELETE /resource
     # def destroy
@@ -61,5 +60,7 @@ module Users
     # def after_inactive_sign_up_path_for(resource)
     #   super(resource)
     # end
+
+
   end
 end
