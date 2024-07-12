66ddac2762aeb5943bd1117204e8bd4582d51b95
taichi <taichishiozawa@gmail.com>
2024-07-12 10:57:15 +0900

    clean: rubocop

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,9 +1,10 @@
-class BookmarksController < ApplicationController
+# frozen_string_literal: true
 
+class BookmarksController < ApplicationController
   def index
     @bookmarking_posts = current_user.bookmarking_posts
                                      .order(created_at: :desc)
-                                     .includes(user: { avatar_attachment: :blob },images_attachments: :blob)
+                                     .includes(user: { avatar_attachment: :blob }, images_attachments: :blob)
                                      .page(params[:page])
                                      .per(10)
   end
