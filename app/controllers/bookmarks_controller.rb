e4938ef69047095aba105e50e9aef006b09aa90d
taichi <taichishiozawa@gmail.com>
2024-07-12 10:28:34 +0900

    add: bookmark#index追加

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,11 @@
 class BookmarksController < ApplicationController
 
   def index
+    @bookmarking_posts = current_user.bookmarking_posts
+                                     .order(created_at: :desc)
+                                     .includes(user: { avatar_attachment: :blob },images_attachments: :blob)
+                                     .page(params[:page])
+                                     .per(10)
   end
 
   def create
