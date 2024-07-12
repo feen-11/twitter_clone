398148840b4d519f00d3c051ffe35c5c2c4c3293
taichi <taichishiozawa@gmail.com>
2024-07-12 10:44:00 +0900

    add: ブックマークボタン追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -20,4 +20,8 @@ class Post < ApplicationRecord
   def reposted_by?(user)
     reposts.where(user_id: user.id).exists?
   end
+
+  def bookmarked_by?(user)
+    bookmarks.where(user_id: user.id).exists?
+  end
 end
