b640e60d9bf7051493343d97b28f71f8d9768d8c
taichi <taichishiozawa@gmail.com>
2024-07-13 14:04:48 +0900

    add: direct_message_room#createを追加

--- app/controllers/direct_message_rooms_controller.rb
+++ app/controllers/direct_message_rooms_controller.rb
@@ -6,5 +6,19 @@ class DirectMessageRoomsController < ApplicationController
   end
 
   def create
+    @direct_message_room = DirectMessageRoom.new
+    if @direct_message_room.save
+      DirectMessageEntry.create(user_id: current_user.id, direct_message_room_id: @direct_message_room.id)
+      DirectMessageEntry.create(user_id: params[:target_user_id], direct_message_room_id: @direct_message_room.id)
+      redirect_to user_direct_message_room_path(current_user, @direct_message_room)
+    else
+      redirect_to user_path(params[:user_id]), alert: 'ダイレクトメッセージルームの作成に失敗しました。'
+    end
+  end
+
+  private
+
+  def direct_message_room_params
+    params.permit(:target_user_id)
   end
 end
