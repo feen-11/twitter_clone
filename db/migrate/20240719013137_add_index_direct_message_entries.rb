c6c25cb3bf09f5269d5e2d66a7f21738f5f6d16a
taichi <taichishiozawa@gmail.com>
2024-07-19 11:01:24 +0900

    clean: rubocop

--- db/migrate/20240719013137_add_index_direct_message_entries.rb
+++ db/migrate/20240719013137_add_index_direct_message_entries.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class AddIndexDirectMessageEntries < ActiveRecord::Migration[7.0]
   def change
     add_index :direct_message_entries, %i[user_id direct_message_room_id],
-      unique: true,
-      name: 'index_dm_entries_on_user_id_and_dm_room_id'
+              unique: true,
+              name: 'index_dm_entries_on_user_id_and_dm_room_id'
   end
 end
