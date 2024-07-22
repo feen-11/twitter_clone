bf4477906732526d541295af930f2e0086c9e8fe
taichi <taichishiozawa@gmail.com>
2024-07-22 21:54:13 +0900

    add: リポストの通知機能を追加

--- app/models/repost.rb
+++ app/models/repost.rb
@@ -3,4 +3,17 @@
 class Repost < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  after_create :create_notification
+
+  private
+
+  def create_notification
+    Notification.create(
+      user: post.user,
+      subject: self,
+      notification_type: 'new_repost',
+      message: "#{user.name}さんがあなたのポストをリポストしました"
+    )
+  end
 end
