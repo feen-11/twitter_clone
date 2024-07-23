0ec9901a1d2d40f8bb57a587eb6f2c7d90d5a6d2
taichi <taichishiozawa@gmail.com>
2024-07-23 22:53:02 +0900

    add: 通知が既読になった場合read_atを更新するよう変更

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,5 +3,8 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications.order(created_at: :desc)
+    @notifications.where(read_at: nil).each do |notification|
+      notification.update_attribute(:read_at, Time.zone.now)
+    end
   end
 end
