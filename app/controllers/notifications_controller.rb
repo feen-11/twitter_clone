f0b9d61b1800a521e425ca6f9c73a083e3b12880
taichi <taichishiozawa@gmail.com>
2024-07-24 13:40:04 +0900

    fix: 通知の既読処理をupdate_allに変更

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,8 +3,6 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications.order(created_at: :desc)
-    @notifications.where(read_at: nil).find_each do |notification|
-      notification.update(read_at: Time.zone.now)
-    end
+    @notifications.where(read_at: nil).update_all(read_at: Time.zone.now)
   end
 end
